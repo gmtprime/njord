89c8e801e5f5dce9aa17647e0c769b470b9f6618
Alexander De Sousa <alexander.jose.de.sousa@gmail.com>
2016-09-05 16:45:29 +0200

    Added support for Elixir 1.2 in the mix.exs file

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Njord.Mixfile do
   def project do
     [app: :njord,
      version: @version,
-     elixir: "~> 1.3",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
